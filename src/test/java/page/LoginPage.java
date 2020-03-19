d2e5c5b9d6fa14748f8efc28230a58c64532e3ca
Aleksandr Nester <Aleksandrnes08@mail.ru>
2020-03-19 16:36:26 +0300

    Deleted unused import and formatted

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -5,21 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
 public class LoginPage extends AbstractPage {
-
     @FindBy(xpath = "//*[@id='username']")
     private WebElement txtUserName;
 
     @FindBy(xpath = "//*[@id='root']//button/span")
     private WebElement btnContinue;
 
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 
+    public LoginPage openPage(String LoginPage) {
+        driver.navigate().to(LoginPage);
+        return this;
+    }
 
     public WebElement getWebElement(String NameOfElement) {
         Map<String, WebElement> elements = new HashMap<String, WebElement>();
@@ -28,17 +32,10 @@ public class LoginPage extends AbstractPage {
         return elements.get(NameOfElement);
     }
 
-    public PasswordPage clickOnButtonContinue(String nameOfButton){
+    public PasswordPage clickOnButtonContinue(String nameOfButton) {
         DriverWaiter.waitToBeClickable(getWebElement(nameOfButton)).click();
         return new PasswordPage(driver);
     }
 
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
 
-    public LoginPage openPage (String LoginPage) {
-        driver.navigate().to(LoginPage);
-        return this;
-    }
 }
