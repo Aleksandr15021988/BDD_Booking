4be649d77d6d7904ce45e65831ae37a750b171e2
Aleksandr15021988 <46677939+Aleksandr15021988@users.noreply.github.com>
2020-03-19 14:33:19 +0300

    Update AbstractPage.java
    Delete time constants

--- src/test/java/page/AbstractPage.java
+++ src/test/java/page/AbstractPage.java
@@ -11,9 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractPage {
-    private final int WAIT_TIME_OUT = 10;
-    private final int LONG_WAIT_TIME_OUT = 180;
-
+    
     protected WebDriver driver;
 
     protected abstract AbstractPage openPage(String pageName);
