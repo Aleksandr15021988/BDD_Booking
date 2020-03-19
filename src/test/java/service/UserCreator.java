045c2aae0bf3f2f9ab9a7ddf7e05ed7643a099e2
Aleksandr Nester <Aleksandrnes08@mail.ru>
2020-03-19 17:09:22 +0300

    formatted

--- src/test/java/service/UserCreator.java
+++ src/test/java/service/UserCreator.java
@@ -5,7 +5,7 @@ import model.UserData;
 public class UserCreator {
 
     public static final String TESTDATA_USER = "testdata.User";
-    public static final String TESTDATA_PASSWORD ="testdata.Password";
+    public static final String TESTDATA_PASSWORD = "testdata.Password";
 
     public static UserData withCredentialsFromProperty() {
         return new UserData(TestDataReader.getTestData(TESTDATA_USER),
