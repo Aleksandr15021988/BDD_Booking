f55fb9e7c8e8c32c9c9e10e880b8115d43bfb829
Aleksandr Nester <Aleksandrnes08@mail.ru>
2020-03-19 17:04:12 +0300

    Relocated constructor and method of super class and formatted

--- src/test/java/page/PasswordPage.java
+++ src/test/java/page/PasswordPage.java
@@ -1,5 +1,6 @@
 package page;
 
+import constants.URL_Constants;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,7 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PasswordPage extends AbstractPage {
-    private final String PASSWORD_PAGE_URL= "https://account.booking.com/sign-in/password";
 
     @FindBy(xpath = "//*[@id='password']")
     private WebElement txtPassword;
@@ -16,25 +16,24 @@ public class PasswordPage extends AbstractPage {
     @FindBy(xpath = "//*[@type='submit']")
     private WebElement btnSignIn;
 
-
-
-    public WebElement getWebElement(String NameOfElement) {
-        Map<String, WebElement> elements = new HashMap<String, WebElement>();
-        elements.put("Password", txtPassword);
-        elements.put("Sign in",btnSignIn);
-        return elements.get(NameOfElement);
-    }
-
     public PasswordPage(WebDriver driver) {
         super(driver);
     }
 
     public PasswordPage openPage(String pageName) {
-        driver.navigate().to(PASSWORD_PAGE_URL);
+        driver.navigate().to(URL_Constants.getURL(pageName));
         return this;
     }
 
-    public MainPage clickOnButtonSignIn(String nameOfButton){
+
+    public WebElement getWebElement(String NameOfElement) {
+        Map<String, WebElement> elements = new HashMap<String, WebElement>();
+        elements.put("Password", txtPassword);
+        elements.put("Sign in", btnSignIn);
+        return elements.get(NameOfElement);
+    }
+    
+    public MainPage clickOnButtonSignIn(String nameOfButton) {
         getWebElement(nameOfButton).click();
         return new MainPage(driver);
     }
