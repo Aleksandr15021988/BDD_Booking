90037a1ec2a382b88717023d3d8afcb00f994970
Aleksandr Nester <Aleksandrnes08@mail.ru>
2020-03-19 16:45:53 +0300

    Relocated constructor and method of super class and formatted and deleted unused method

--- src/test/java/page/LondonHotelsPage.java
+++ src/test/java/page/LondonHotelsPage.java
@@ -1,6 +1,5 @@
 package page;
 
-import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,22 +13,19 @@ public class LondonHotelsPage extends AbstractPage {
     @FindBy(xpath = "//span[contains(.,'See availability')]")
     private List<WebElement> btnsSelectYourRoom;
 
-    public WebElement getWebElement(String NameOfElement) {
-        Map<String, WebElement> elements = new HashMap<String, WebElement>();
-        elements.put("Select Your Room",btnsSelectYourRoom.get(0));
-        return elements.get(NameOfElement);
-    }
-
-    public FirstHotelPage clickButtonSeeAvailability(String button){
-        getWebElement(button).click();
-        return new FirstHotelPage(DriverSingleton.getDriver());
-    }
-
-    public LondonHotelsPage openPage (String LoginPage) {
+    public LondonHotelsPage openPage(String LoginPage) {
         driver.navigate().to(LoginPage);
         return this;
     }
+
     public LondonHotelsPage(WebDriver driver) {
         super(driver);
     }
+
+    public WebElement getWebElement(String NameOfElement) {
+        Map<String, WebElement> elements = new HashMap<String, WebElement>();
+        elements.put("Select Your Room", btnsSelectYourRoom.get(0));
+        return elements.get(NameOfElement);
+    }
+    
 }
