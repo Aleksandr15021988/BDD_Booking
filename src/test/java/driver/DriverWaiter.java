88a1b6e54da0846b34600d96a1d75a2bfed86469
Aleksandr Nester <Aleksandrnes08@mail.ru>
2020-03-19 16:28:17 +0300

    formatted code

--- src/test/java/driver/DriverWaiter.java
+++ src/test/java/driver/DriverWaiter.java
@@ -1,23 +1,20 @@
 package driver;
 
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.List;
 
 public class DriverWaiter {
 
     private static final int TIME_OUT = 10;
 
-    public static WebElement waitToBeClickable(WebElement element){
-        return  new WebDriverWait(DriverSingleton.getDriver(), TIME_OUT)
+    public static WebElement waitToBeClickable(WebElement element) {
+        return new WebDriverWait(DriverSingleton.getDriver(), TIME_OUT)
                 .until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public static WebElement waitToBePresenceOfElementLocated(WebElement element){
-        return  new WebDriverWait(DriverSingleton.getDriver(), TIME_OUT)
+    public static WebElement waitToBePresenceOfElementLocated(WebElement element) {
+        return new WebDriverWait(DriverSingleton.getDriver(), TIME_OUT)
                 .until(ExpectedConditions.visibilityOf(element));
     }
 }
